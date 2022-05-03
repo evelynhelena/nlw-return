2c9cd54a0b5f3017a8589e582235d2c6d290b5e5
evelynhelena <evelynhelena781@gmail.com>
2022-05-02 23:00:54 -0300

    Iniciando projeto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors:{
+        brand:{
+          500:'#8557e6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
